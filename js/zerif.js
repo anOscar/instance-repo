2e9e7efd84c70e848c581ddf114f3313dccb5e63
Oscar Rojas <oscarrojas3896@gmail.com>
2015-04-04 22:33:41 -0400

    added new background images

--- js/zerif.js
+++ js/zerif.js
@@ -5,8 +5,8 @@ $.vegas('slideshow', {
   delay:7000,
   backgrounds:[
     { src:'images/backgrounds/bg1.png', fade:1000 },
-    { src:'images/backgrounds/bg1.jpg', fade:1000 },
-    { src:'images/backgrounds/bg1.jpg', fade:1000 }
+    { src:'images/backgrounds/bg2.jpg', fade:1000 },
+    { src:'images/backgrounds/bg3.jpg', fade:1000 }
   ]
 })('overlay');
       
